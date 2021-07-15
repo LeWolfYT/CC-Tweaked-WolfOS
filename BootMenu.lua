5ac9cc36f6e9f8005fc83e4c776232192cccb19a
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2021-07-15 19:19:25 -0400

    Create BootMenu.lua

---
+++ BootMenu.lua
@@ -0,0 +1,25 @@
+term.setCursorPos(8,2)
+term.write("WolfOS Boot menu 1.0")
+
+term.setCursorPos(8,4)
+term.write("[S]tart WolfOS 1.0")
+term.setCursorPos(8,6)
+term.write("[E]xit to CraftOS")
+term.setCursorPos(8,8)
+term.write("[U]ninstall WolfOS 1.0")
+
+while true do
+  local e,p = os.pullEvent()
+    if e == "key" then
+      local key = p
+      if key == 83 then
+        shell.run("/WolfOS/Menu.lua")
+      elseif key == 85 then
+        shell.run("/WolfOS/Uninstall.lua")
+      elseif key == 69 then
+        break
+      else
+        break
+      end
+   end
+end

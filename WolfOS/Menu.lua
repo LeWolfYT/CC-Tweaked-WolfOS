49ecff71c85e1228688e9bd363fe37eb164a6579
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2021-07-16 16:03:35 -0400

    Update Menu.lua

--- WolfOS/Menu.lua
+++ WolfOS/Menu.lua
@@ -1,1 +1,13 @@
-print("a")
+term.setCursorPos(1,1)
+term.setBackgroundColor(colors.cyan)
+term.setTextColor(colors.cyan)
+for i == 1, 17 do
+  print("###################################################")
+end
+term.setCursorPos(1,19)
+term.setBackgroundColor(colors.lightBlue)
+term.setTextColor(colors.lightBlue)
+print("###################################################")
+term.setCursorPos(46,18)
+term.setTextColor(colors.black)
+term.write(textutils.formatTime(os.time(), true))

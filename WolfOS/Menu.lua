43c12d0ecd3641f7c0fe6f2296df6c3535d5ca33
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2021-07-16 16:23:32 -0400

    Update Menu.lua

--- WolfOS/Menu.lua
+++ WolfOS/Menu.lua
@@ -1,13 +1,17 @@
 term.setCursorPos(1,1)
 term.setBackgroundColor(colors.cyan)
 term.setTextColor(colors.cyan)
-for i == 1, 17 do
+for i == 1, 19 do
   print("###################################################")
 end
-term.setCursorPos(1,19)
-term.setBackgroundColor(colors.lightBlue)
-term.setTextColor(colors.lightBlue)
+term.setCursorPos(1,20)
+term.setBackgroundColor(colors.lightGray)
+term.setTextColor(colors.lightGray)
 print("###################################################")
 term.setCursorPos(46,18)
 term.setTextColor(colors.black)
 term.write(textutils.formatTime(os.time(), true))
+term.setCursorPos(51,19)
+term.write(" ")
+term.setCursorPos(1,19)
+term.write("Menu (Shift+M)")

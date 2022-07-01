002c746ed4df7c8060b9f1a68171944fe58a622c
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2022-07-01 19:46:22 -0400

    Add files via upload

--- WolfOS/Rprogs.lua
+++ WolfOS/Rprogs.lua
@@ -10,19 +10,14 @@ term.setCursorPos(1,5)
 term.setTextColor(colors.black)
 progs = fs.list("/WolfOS/Programs")
 proglen = #progs
-function centerx(text)
-  local txl = #text
-  local txlg = txl / 2
-  local txls = math.floor(txlg)
-  local xpos = (sizex / 2) - txls
-  return(xpos)
-end
 
 function addtx(text, ypos)
-  term.setCursorPos(centerx(text), ypos)
+  term.setCursorPos(2, ypos)
   term.write(text)
 end
 
-for i in 1,proglen do
-  addtx(i .. ". " .. progs[i], ((2 * i) + 2) )
+addtx("Programs (<X> to exit)", 2)
+
+for i = 1,proglen do
+    addtx(i .. ". " .. progs[i], (2*i+2))
 end

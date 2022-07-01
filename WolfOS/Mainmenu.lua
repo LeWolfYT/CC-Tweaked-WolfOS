bb153a6b07fd17b5d0e225412e844c5e45eb8743
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2022-06-30 20:03:35 -0400

    Create Mainmenu.lua

---
+++ WolfOS/Mainmenu.lua
@@ -0,0 +1,17 @@
+term.clear()
+term.setCursorPos(1,1)
+term.setBackgroundColor(colors.lime)
+term.setTextColor(colors.orange)
+sizex, sizey = term.getSize()
+for i=1,sizey do
+  print(string.rep("#", sizex))
+end
+term.setTextColor(colors.black)
+function centerx(text, size)
+  local txl = #text
+  local txlg = txl / 2
+  local txls = math.floor(txlg)
+  local center = math.floor(sizex / 2)
+  local xpos = txls - center
+  return(xpos)
+end

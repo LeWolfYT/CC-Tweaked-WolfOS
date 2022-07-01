a36fa5b709f5f776de72e339c1940d13c2aa96a2
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2022-07-01 09:29:13 -0400

    Update Mainmenu.lua

--- WolfOS/Mainmenu.lua
+++ WolfOS/Mainmenu.lua
@@ -23,3 +23,20 @@ end
 addtx("1. Cancel", 2)
 addtx("2. Exit to CraftOS", 4)
 addtx("3. Download programs", 6)
+
+while true do
+  local e,p = os.pullEvent()
+    if e == "key" then
+      local key = p
+      if key == keys.one then
+        exit()
+      elseif key == keys.two then
+        shell.run("/WolfOS/Uninstall.lua")
+      elseif key == keys.three then
+        shell.run("/WolfOS/Dprogs.lua")
+        break
+      else
+        break
+      end
+   end
+end

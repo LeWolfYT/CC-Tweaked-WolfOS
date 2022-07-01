c92356b198d04ae7f3ac07d1149d7c145f7d6ed9
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2022-06-30 20:38:32 -0400

    Update Mainmenu.lua

--- WolfOS/Mainmenu.lua
+++ WolfOS/Mainmenu.lua
@@ -7,7 +7,7 @@ for i=1,sizey do
   print(string.rep("#", sizex))
 end
 term.setTextColor(colors.black)
-function centerx(text, size)
+function centerx(text)
   local txl = #text
   local txlg = txl / 2
   local txls = math.floor(txlg)
@@ -15,3 +15,11 @@ function centerx(text, size)
   local xpos = txls - center
   return(xpos)
 end
+
+function addtx(text, ypos)
+  term.setCursorPos(centerx, ypos)
+  term.write(text)
+
+addtx("1. Cancel", 2)
+addtx("2. Exit to CraftOS", 4)
+addtx("3. Download programs", 6)

b16a91a2d5e1e9496212d7772cf1b87537eca16a
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2022-07-02 11:40:26 -0400

    Update Rprogs.lua

--- WolfOS/Rprogs.lua
+++ WolfOS/Rprogs.lua
@@ -21,3 +21,29 @@ addtx("Programs (<X> to exit)", 2)
 for i = 1,proglen do
     addtx(i .. ". " .. progs[i], (2*i+2))
 end
+
+function has_value(tab, val, prognum)
+    for index, value in ipairs(tab) do
+        if value == val then
+            return true
+        end
+    end
+
+    return false
+end
+
+numkeys = {keys.one, keys.two, keys.three, keys.four, keys.five, keys.six, keys.seven, keys.eight, keys.nine}
+
+while true do 
+  local e,p = os.pullEvent()
+    if e == "key" then
+      local key = p
+      if key == keys.x then
+        shell.run("/WolfOS/Menu.lua")
+      elseif has_value(numkeys, p, proglen) and not has_value(numkeys, (p - (9 - proglen)), proglen) then
+        shell.run("/WolfOS/Programs/" .. progs[p - 48])
+      else
+        break
+      end
+   end
+end

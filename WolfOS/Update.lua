d01818c4cce65acb73227c16579869f4c79b6c92
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2022-07-02 15:11:30 -0400

    Update Update.lua

--- WolfOS/Update.lua
+++ WolfOS/Update.lua
@@ -1,4 +1,4 @@
 shell.run("cd /WolfOS")
 shell.run("delete Reinstaller.lua")
 shell.run("wget https://raw.githubusercontent.com/LeWolfYT/CC-Tweaked-WolfOS/main/WolfOS/Reinstaller.lua")
-shell.run("Renstaller")
+shell.run("Reinstaller")

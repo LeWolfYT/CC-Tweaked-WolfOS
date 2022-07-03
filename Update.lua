fba555a61a985e28c0210fe668932f5fa11ad5da
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2022-07-03 11:51:49 -0400

    Update Update.lua

--- Update.lua
+++ Update.lua
@@ -1,3 +1,3 @@
 shell.run("delete Install.lua")
-shell.run("wget https://raw.githubusercontent.com/LeWolfYT/CC-Tweaked-WolfOS/main/Install.lua")
+shell.run("wget https://raw.githubusercontent.com/LeWolfYT/CC-Tweaked-WolfOS/main/Install.lua Install.lua")
 shell.run("Install")
